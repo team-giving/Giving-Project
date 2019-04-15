8167b5fee8af54eed6970b7d200ea9274b486526
Shash Sinha <shash678@outlook.com>
2019-04-15 14:28:51 -0400

    Added iOS splash screen

--- Giving/ios/Giving/AppDelegate.m
+++ Giving/ios/Giving/AppDelegate.m
@@ -21,12 +21,12 @@
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [[NSRunLoop currentRunLoop]runUntilDate:[NSDate dateWithTimeIntervalSinceNow: 1.0]];
   return YES;
 }
 
