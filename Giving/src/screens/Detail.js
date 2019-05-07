4ef402aecc336a0a14aa5e639dee278e4d9adee3
Shash Sinha <shash678@outlook.com>
2019-05-07 11:26:48 -0400

    Merge: Mustafa's Detail Page

--- Giving/src/screens/Detail.js
+++ Giving/src/screens/Detail.js
@@ -1,43 +1,63 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Linking } from 'react-native';
 import { FavoriteButton } from "../components/favorite-button.js";
-import { RatingImage }  from "../components/rating-image.js";
-
+import { RatingImage } from "../components/rating-image.js";
+import { TouchableOpacity } from 'react-native-gesture-handler';
 export default class Detail extends Component {
-
     static navigationOptions = ({ navigation }) => {
         const charityData = navigation.getParam('charityData');
-    return {
-      title: charityData.charityName,
+        return {
+            title: charityData.charityName,
+        };
     };
-  };
-
+    donate = (link) => {
+        Linking.openURL(link);
+    }
     render() {
         const charityData = this.props.navigation.getParam('charityData');
-        const favList = this.props.navigation.getParam('favList');
         console.log(charityData);
         return (
             <View style={styles.container}>
-                <Text> Charity Name: {charityData.charityName} </Text>
-                <Text> EIN: {charityData.ein} </Text>
-                <Text> Charity Nav URL: {charityData.charityNavigatorURL} </Text>
-                <Text> Category: {charityData.category.categoryName}</Text>
-                <Text> Website URL: {charityData.websiteURL} </Text>
-                <Text> Tagline: {charityData.tagLine}</Text>
-                <Text> Mission: {charityData.mission}</Text>
-                <FavoriteButton ein={charityData.ein} favoritesList={favList}/>
-                <RatingImage currentRating={charityData.currentRating}/>
-
+                <View style={{ display: 'flex', flexDirection: 'row' }}>
+                    <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 30, flex: 0.8 }}> {charityData.charityName} </Text>
+                    <FavoriteButton style={{ flex: 0.2, marginTop:20 }} ein={charityData.ein} favoritesList={[]} />
+                </View>
+                <TouchableOpacity style={styles.buttonContainer} onPress={() => this.donate(charityData.websiteURL)}>
+                        <Text style={styles.buttonText}> Donate Now </Text>
+                </TouchableOpacity>
+                <RatingImage currentRating={charityData.currentRating} />
+                {/* <Text style={styles.textContainer}> EIN: {charityData.ein} </Text> */}
+                {/* <Text style={styles.textContainer}> Charity Nav URL: {charityData.charityNavigatorURL} </Text> */}
+                <Text style={styles.textContainer}> {charityData.category.categoryName}</Text>
+                {/* <Text style={styles.textContainer}> Website URL: {charityData.websiteURL} </Text> */}
+                <Text style={styles.textContainer}> Tagline: {charityData.tagLine}</Text>
+                <Text style={styles.textContainer}> Mission: {charityData.mission}</Text>
             </View>
         );
     }
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#1578d0',
+    },
+    textContainer: {
+        color: 'white',
+        fontSize: 12,
+        margin: 10
+    },
+    buttonContainer: {
+        backgroundColor: '#1578d0',
+        paddingVertical: 12,
+        margin: 10,
+        borderRadius: 30,
+        // width: window.width - 30
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontSize: 16
     }
 });
